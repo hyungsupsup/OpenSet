3ab4f59c204266fc6e0fb214836aa5a6ad413c6d
hyungsup <84906047+hyungsupsup@users.noreply.github.com>
2022-02-17 23:35:23 +0900

    Update openmax_utils.py

--- utils/openmax_utils.py
+++ utils/openmax_utils.py
@@ -28,9 +28,7 @@ def getlabellist(synsetfname):
     return labellist
 
 
-def compute_distance(
-        query_channel, channel, mean_vec,
-        distance_type='eucos'):
+def compute_distance(query_channel, channel, mean_vec, distance_type='eucos'):
     """ Compute the specified distance type between chanels of mean vector
     and query image. In caffe library, FC8 layer consists of 10 channels.
     Here, we compute distance of distance of each channel (from query image)
@@ -56,9 +54,13 @@ def compute_distance(
     # print ('query_ch',query_channel)
     # print ('channel',channel)
 
-    query_channel = np.array(query_channel)
-    mean_vec = np.reshape(mean_vec, (5, 1))
-    # print ('shape',mean_vec.shape)
+    query_channel = np.array(query_channel).flatten()
+    print('flattened query_channel: ', query_channel.shape)
+    print('shape: ', mean_vec.shape)
+    #mean_vec = np.reshape(mean_vec, (5, 1))   # 여기서 에러
+    mean_vec = mean_vec.flatten()
+    print('flattened shape: ', mean_vec.shape)
+
     # exit()
     # print (distance_type)
     if distance_type == 'eucos':
